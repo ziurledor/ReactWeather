395ffef60cb2300c00faeeb1794fec528948d2d0
Rodel Ruiz <Rodel Ruiz>
2016-11-01 22:09:53 +0300

    Initial commit

---
+++ examples-arrow-functions.js
@@ -0,0 +1,25 @@
+//var names = ['Rodel', 'Libay', 'Rj', 'Mae'];
+
+//names.forEach(function(name) {
+//    console.log('forEach', name);
+//});
+
+//names.forEach((name) => {
+//    console.log('arrowFunc', name);
+//});
+
+//names.forEach((name) => console.log(name));
+
+//var returnMe = (name) => name + '!';
+//console.log(returnMe('Rodel'));
+
+//var person = {
+//    name: 'Rodel',
+//    greet: function() {
+//        names.forEach((name) => {
+//            console.log(this.name + ' says hi to ' + name)
+//        });
+//    }
+//};
+
+//person.greet();
