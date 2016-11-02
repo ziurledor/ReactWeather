bbb5cf9a3cb9e84926182de3ec89e8af8bcba4c8
Rodel Ruiz <Rodel Ruiz>
2016-11-03 00:02:35 +0300

    Update middleware for localhost

--- public/bundle.js
+++ public/bundle.js
@@ -27160,9 +27160,18 @@
 
 	var Examples = function Examples(props) {
 	   return React.createElement(
-	      'h3',
+	      'div',
 	      null,
-	      'Examples Component'
+	      React.createElement(
+	         'h3',
+	         null,
+	         'Examples Component!'
+	      ),
+	      React.createElement(
+	         'p',
+	         null,
+	         'Welcome to examples page!'
+	      )
 	   );
 	};
 
