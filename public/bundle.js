01feec60b59c804e1beb29879e1dc6ff09e7fd76
Rodel Ruiz <Rodel Ruiz>
2016-11-03 00:37:04 +0300

    Update about page copy

--- public/bundle.js
+++ public/bundle.js
@@ -27142,9 +27142,18 @@
 
 	var About = function About(props) {
 	   return React.createElement(
-	      'h3',
+	      'div',
 	      null,
-	      'About Component'
+	      React.createElement(
+	         'h3',
+	         null,
+	         'About'
+	      ),
+	      React.createElement(
+	         'p',
+	         null,
+	         'Welcome to the about page.'
+	      )
 	   );
 	};
 
